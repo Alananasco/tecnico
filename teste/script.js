7b10dcfdbbc8afe2570687257d90b72de3217b0b
ALANA DO NASCIMENTO <00001098869126sp@aluno.educacao.sp.gov.br>
2024-07-30 11:07:27 -0300

    oi

--- teste/script.js
+++ teste/script.js
@@ -1,13 +1,43 @@
-document.getElementById('loginform').addEventListener('submit', function login (event) {
+const usuario = [
+
+    {
+        usuario: "allie",
+        senha: "123"
+    },
+
+    {
+        usuario: "skot",
+        senha: "456"
+    },
+
+    {
+        usuario: "ryan",
+        senha: "789"
+    },
+
+    {
+        usuario: "oi",
+        senha: "thau"
+    }
+]
+
+document.getElementById('loginform').addEventListener('submit',function login(event){
     event.preventDefault();
-   
+ 
     var user = document.getElementById('user').value;
     var senha = document.getElementById('senha').value;
-   
-    if (user === "allie" && senha === "123"){
-        alert('sucesso');
-        location.href = "home.html";  
-    } else {
-        alert('usuario ou senha incorreto');
+    var loginValido = false;
+ 
+    for (var i in usuario){
+        if (user == usuario[i].usuario && senha == usuario[i].senha){
+            loginValido = true;
+            break;
     }
-    });
+}
+ 
+    if(loginValido == true){
+        alert('sucesso');
+        location.href = "home.html";
+    }else{
+        alert('usuario ou senha incorreta');
+    }});
